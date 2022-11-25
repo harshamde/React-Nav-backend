2b5a37c060726ba7acd6d92331b5a14f4c25ec89
harshamde <harshamdo619@gmail.com>
2022-11-25 11:29:44 +0530

    completed register route with hash

--- server.js
+++ server.js
@@ -1,15 +1,43 @@
 const express = require("express");
 const { MongoClient, ObjectId } = require('mongodb');
-const process = require('node:process');
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 const client = new MongoClient('mongodb://localhost:27017');
 const dataBase = client.db('db1');
+const SECRET_KEY_FOR_JWT = "jsonWebToken12uanjd565";
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const createJwt = () => {
+    const newJwt = jwt.sign(
+        {
+            username: "aaa",
+            password: "aaa123#",
+        },
+        SECRET_KEY_FOR_JWT,
+        { expiresIn: '1h' }
+    );
+};
+
+const verifyJwt = (token) => {
+    const newJwt = jwt.verify(token, SECRET_KEY_FOR_JWT, (error, decode) => {
+        if (error) {
+            console.log("error");
+        } else {
+            console.log(decode);
+        }
+    });
+}
+
 
 app.get("/get-books", async (request, response) => {
+
+    // const token = request.headers.authorization.split("Bearer")[1];
+    // // console.log(token);
+    // verifyJwt(token);
     try {
         const collection = dataBase.collection('books');
         const books = await collection.find().toArray();
@@ -19,7 +47,6 @@ app.get("/get-books", async (request, response) => {
     } catch (error) {
         console.log(error);
     }
-
 });
 
 
@@ -47,7 +74,9 @@ app.post('/save-books', async (request, response) => {
             }
         }
         const updatedBooks = await collection.find().toArray();
-        response.json(updatedBooks);
+        setTimeout(() => {
+            response.json(updatedBooks);
+        }, 2000);
     } catch (error) {
         console.log(error)
     }
@@ -58,6 +87,7 @@ app.get("/get-products", async (request, response) => {
     try {
         const collection = dataBase.collection('products');
         const products = await collection.find().toArray();
+        console.log(products);
         setTimeout(() => {
             response.json(products);
         }, 2000);
@@ -91,9 +121,46 @@ app.post('/save-products', async (request, response) => {
             }
         }
         const updatedProducts = await collection.find().toArray();
-        response.json(updatedProducts);
+        setTimeout(() => {
+            response.json(updatedProducts);
+        }, 2000);
     } catch (error) {
         console.log(error)
+    }
+});
+
+
+app.post('/register', async (request, response) => {
+    try {
+        const { username, password, confirmPassword } = request.body;
+        if (username.length < 3) {
+            response.json({ status: "FAILED", error: "Validation error", message: "Username should have at least 3 characters." });
+            return;
+        }
+        if (password !== confirmPassword) {
+            response.json({ status: "FAILED", error: "Validation error", message: "Password and confirm password should be same." });
+            return;
+        }
+        if (password.length < 6) {
+            response.json({ status: "FAILED", error: "Validation error", message: "Password should have at least 6 characters." });
+            return;
+        }
+
+        const collection = dataBase.collection("users");
+        const usernameExist = await collection.findOne({ username });
+
+        if (usernameExist) {
+            response.json({ status: "FAILED", error: "Validation error", message: "Username already exist." });
+            return;
+        }
+
+        const hash = await bcrypt.hash(password, saltRounds);
+
+        await collection.insertOne({ username, password: hash });
+        response.json({ status: "SUCCESS", message: "Registered successfully."});
+
+    } catch (error) {
+        response.json(error);
     }
 });
 
